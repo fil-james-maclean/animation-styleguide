0704d994518cd77f754be58e99b391fdb52683f5
Maclean, James <James.Maclean@fil.com>
2017-01-18 16:59:00 +0000

    lots of animations setup
    3d card flip is a bit different

--- app/js/custom.js
+++ app/js/custom.js
@@ -73,3 +73,121 @@ $( document ).on( 'click', '.js-trigger6', function( e ) {
      e.preventDefault();
 
 } );
+//
+// $( document ).on( 'click', '.js-trigger7', function( e ) {
+//
+//      $( '.js-target7' ).each( function() {
+//
+//           $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-500 fil-ani-FlipLeft180' );
+//
+//      } );
+//
+//      e.preventDefault();
+//
+// } );
+
+
+$( document ).on( 'click', '.js-trigger8', function( e ) {
+
+     $( '.js-target8' ).each( function() {
+
+          $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeOut fil-ani-is-hidden' );
+
+     } );
+
+     e.preventDefault();
+
+ } );
+
+$( document ).on( 'click', '.js-trigger9', function( e ) {
+
+  $( '.js-target9' ).each( function() {
+
+       $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeOutDown fil-ani-is-hidden' );
+
+  } );
+
+  e.preventDefault();
+
+} );
+
+
+$( document ).on( 'click', '.js-trigger10', function( e ) {
+
+     $( '.js-target10' ).each( function() {
+
+          $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeOutLeft fil-ani-is-hidden' );
+
+     } );
+
+     e.preventDefault();
+
+} );
+
+
+$( document ).on( 'click', '.js-trigger11', function( e ) {
+
+     $( '.js-target11' ).each( function() {
+
+          $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeOutRight fil-ani-is-hidden' );
+
+     } );
+
+     e.preventDefault();
+
+} );
+
+
+$( document ).on( 'click', '.js-trigger12', function( e ) {
+
+     $( '.js-target12' ).each( function() {
+
+          $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-500 fil-ani-FlipOutUp90 fil-ani-is-hidden' );
+
+     } );
+
+     e.preventDefault();
+
+} );
+
+$( document ).on( 'click', '.js-trigger13', function( e ) {
+
+     $( '.js-target13' ).each( function() {
+
+          $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-500 fil-ani-FlipOutDown90 fil-ani-is-hidden' );
+
+     } );
+
+     e.preventDefault();
+
+} );
+
+$( document ).on( 'click', '.js-trigger14', function( e ) {
+
+     $( '.js-target14' ).each( function() {
+
+         if ( $( this ).hasClass( 'fil-ani-card__flipped' ) ) {
+              $( this ).removeClass( 'fil-ani-delay-100 fil-ani-dur-500 fil-ani-FlipLeft180 fil-ani-card__flipped' );
+              $( this ).addClass( 'fil-ani-delay-100 fil-ani-dur-500 fil-ani-FlipRight180' );
+         } else {
+             $( this ).removeClass( 'fil-ani-delay-100 fil-ani-dur-500 fil-ani-FlipRight180' );
+             $( this ).addClass( 'fil-ani-delay-100 fil-ani-dur-500 fil-ani-FlipLeft180 fil-ani-card__flipped' );
+         }
+     } );
+
+     e.preventDefault();
+
+} );
+
+
+$( document ).on( 'click', '.js-trigger15', function( e ) {
+
+     $( '.js-target15' ).each( function() {
+
+          $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-1000 fil-ani-PulseScale' );
+
+     } );
+
+     e.preventDefault();
+
+} );
