c58b0f350e318ef744c3c010d3efa9b61b8fcda0
Maclean, James <James.Maclean@fil.com>
2017-02-07 10:47:11 +0000

    renamed all js classes

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,7 +1,8 @@
 
-$( document ).on( 'click', '.js-trigger1', function( e ) {
 
-     $( '.js-target1' ).each( function() {
+$( document ).on( 'click', '.js-fil-ani-fadeIn-trigger', function( e ) {
+
+     $( '.js-fil-ani-fadeIn-target' ).each( function() {
 
           $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeIn fil-ani-is-hidden' );
 
@@ -12,9 +13,10 @@ $( document ).on( 'click', '.js-trigger1', function( e ) {
  } );
 
 
-$( document ).on( 'click', '.js-trigger2', function( e ) {
 
-    $( '.js-target2' ).each( function() {
+$( document ).on( 'click', '.js-fil-ani-fadeInScaleUp-trigger', function( e ) {
+
+    $( '.js-fil-ani-fadeInScaleUp-target' ).each( function() {
 
          $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-500 fil-ani-fadeInScaleUp fil-ani-is-hidden' );
 
@@ -25,9 +27,10 @@ $( document ).on( 'click', '.js-trigger2', function( e ) {
 } );
 
 
-$( document ).on( 'click', '.js-trigger17', function( e ) {
 
-    $( '.js-target17' ).each( function() {
+$( document ).on( 'click', '.js-fil-ani-fadeInScaleDown-trigger', function( e ) {
+
+    $( '.js-fil-ani-fadeInScaleDown-target' ).each( function() {
 
          $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-500 fil-ani-fadeInScaleDown fil-ani-is-hidden' );
 
@@ -38,9 +41,10 @@ $( document ).on( 'click', '.js-trigger17', function( e ) {
 } );
 
 
-$( document ).on( 'click', '.js-trigger18', function( e ) {
 
-    $( '.js-target18' ).each( function() {
+$( document ).on( 'click', '.js-fil-ani-fadeInUp-trigger', function( e ) {
+
+    $( '.js-fil-ani-fadeInUp-target' ).each( function() {
 
          $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeInUp fil-ani-is-hidden' );
 
@@ -51,9 +55,10 @@ $( document ).on( 'click', '.js-trigger18', function( e ) {
 } );
 
 
-$( document ).on( 'click', '.js-trigger19', function( e ) {
 
-    $( '.js-target19' ).each( function() {
+$( document ).on( 'click', '.js-fil-ani-fadeInDown-trigger', function( e ) {
+
+    $( '.js-fil-ani-fadeInDown-target' ).each( function() {
 
          $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeInDown fil-ani-is-hidden' );
 
@@ -64,9 +69,10 @@ $( document ).on( 'click', '.js-trigger19', function( e ) {
 } );
 
 
-$( document ).on( 'click', '.js-trigger3', function( e ) {
 
-     $( '.js-target3' ).each( function() {
+$( document ).on( 'click', '.js-fil-ani-fadeInLeft-trigger', function( e ) {
+
+     $( '.js-fil-ani-fadeInLeft-target' ).each( function() {
 
           $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeInLeft fil-ani-is-hidden' );
 
@@ -77,9 +83,10 @@ $( document ).on( 'click', '.js-trigger3', function( e ) {
  } );
 
 
- $( document ).on( 'click', '.js-trigger4', function( e ) {
 
-      $( '.js-target4' ).each( function() {
+ $( document ).on( 'click', '.js-fil-ani-fadeInRight-trigger', function( e ) {
+
+      $( '.js-fil-ani-fadeInRight-target' ).each( function() {
 
            $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeInRight fil-ani-is-hidden' );
 
@@ -89,9 +96,10 @@ $( document ).on( 'click', '.js-trigger3', function( e ) {
 
 } );
 
-$( document ).on( 'click', '.js-trigger5', function( e ) {
 
-     $( '.js-target5' ).each( function() {
+$( document ).on( 'click', '.js-fil-ani-FlipUp90-trigger', function( e ) {
+
+     $( '.js-fil-ani-FlipUp90-target' ).each( function() {
 
           $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-500 fil-ani-FlipUp90  fil-ani-is-hidden' );
 
@@ -101,9 +109,10 @@ $( document ).on( 'click', '.js-trigger5', function( e ) {
 
 } );
 
-$( document ).on( 'click', '.js-trigger6', function( e ) {
 
-     $( '.js-target6' ).each( function() {
+$( document ).on( 'click', '.js-fil-ani-FlipDown90-trigger', function( e ) {
+
+     $( '.js-fil-ani-FlipDown90-target' ).each( function() {
 
           $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-500 fil-ani-FlipDown90  fil-ani-is-hidden' );
 
@@ -112,23 +121,11 @@ $( document ).on( 'click', '.js-trigger6', function( e ) {
      e.preventDefault();
 
 } );
-//
-// $( document ).on( 'click', '.js-trigger7', function( e ) {
-//
-//      $( '.js-target7' ).each( function() {
-//
-//           $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-500 fil-ani-FlipLeft180' );
-//
-//      } );
-//
-//      e.preventDefault();
-//
-// } );
 
 
-$( document ).on( 'click', '.js-trigger8', function( e ) {
+$( document ).on( 'click', '.js-fil-ani-fadeOut-trigger', function( e ) {
 
-     $( '.js-target8' ).each( function() {
+     $( '.js-fil-ani-fadeOut-target' ).each( function() {
 
           $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeOut fil-ani-is-hidden' );
 
@@ -138,9 +135,10 @@ $( document ).on( 'click', '.js-trigger8', function( e ) {
 
  } );
 
-$( document ).on( 'click', '.js-trigger9', function( e ) {
 
-  $( '.js-target9' ).each( function() {
+$( document ).on( 'click', '.js-fil-ani-fadeOutScaleDown-trigger', function( e ) {
+
+  $( '.js-fil-ani-fadeOutScaleDown-target' ).each( function() {
 
        $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeOutScaleDown fil-ani-is-hidden' );
 
@@ -150,9 +148,10 @@ $( document ).on( 'click', '.js-trigger9', function( e ) {
 
 } );
 
-$( document ).on( 'click', '.js-trigger22', function( e ) {
 
-  $( '.js-target22' ).each( function() {
+$( document ).on( 'click', '.js-fil-ani-fadeOutScaleUp-trigger', function( e ) {
+
+  $( '.js-fil-ani-fadeOutScaleUp-target' ).each( function() {
 
        $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeOutScaleUp fil-ani-is-hidden' );
 
@@ -162,9 +161,10 @@ $( document ).on( 'click', '.js-trigger22', function( e ) {
 
 } );
 
-$( document ).on( 'click', '.js-trigger20', function( e ) {
 
-  $( '.js-target20' ).each( function() {
+$( document ).on( 'click', '.js-fil-ani-fadeOutUp-trigger', function( e ) {
+
+  $( '.js-fil-ani-fadeOutUp-target' ).each( function() {
 
        $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeOutUp fil-ani-is-hidden' );
 
@@ -174,9 +174,10 @@ $( document ).on( 'click', '.js-trigger20', function( e ) {
 
 } );
 
-$( document ).on( 'click', '.js-trigger21', function( e ) {
 
-  $( '.js-target21' ).each( function() {
+$( document ).on( 'click', '.js-fil-ani-fadeOutDown-trigger', function( e ) {
+
+  $( '.js-fil-ani-fadeOutDown-target' ).each( function() {
 
        $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeOutDown fil-ani-is-hidden' );
 
@@ -187,9 +188,10 @@ $( document ).on( 'click', '.js-trigger21', function( e ) {
 } );
 
 
-$( document ).on( 'click', '.js-trigger10', function( e ) {
 
-     $( '.js-target10' ).each( function() {
+$( document ).on( 'click', '.js-fil-ani-fadeOutLeft-trigger', function( e ) {
+
+     $( '.js-fil-ani-fadeOutLeft-target' ).each( function() {
 
           $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeOutLeft fil-ani-is-hidden' );
 
@@ -200,9 +202,10 @@ $( document ).on( 'click', '.js-trigger10', function( e ) {
 } );
 
 
-$( document ).on( 'click', '.js-trigger11', function( e ) {
 
-     $( '.js-target11' ).each( function() {
+$( document ).on( 'click', '.js-fil-ani-fadeOutRight-trigger', function( e ) {
+
+     $( '.js-fil-ani-fadeOutRight-target' ).each( function() {
 
           $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeOutRight fil-ani-is-hidden' );
 
@@ -213,9 +216,10 @@ $( document ).on( 'click', '.js-trigger11', function( e ) {
 } );
 
 
-$( document ).on( 'click', '.js-trigger12', function( e ) {
 
-     $( '.js-target12' ).each( function() {
+$( document ).on( 'click', '.js-fil-ani-FlipOutUp90-trigger', function( e ) {
+
+     $( '.js-fil-ani-FlipOutUp90-target' ).each( function() {
 
           $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-500 fil-ani-FlipOutUp90 fil-ani-is-hidden' );
 
@@ -225,9 +229,10 @@ $( document ).on( 'click', '.js-trigger12', function( e ) {
 
 } );
 
-$( document ).on( 'click', '.js-trigger13', function( e ) {
 
-     $( '.js-target13' ).each( function() {
+$( document ).on( 'click', '.js-fil-ani-FlipOutDown90-trigger', function( e ) {
+
+     $( '.js-fil-ani-FlipOutDown90-target' ).each( function() {
 
           $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-500 fil-ani-FlipOutDown90 fil-ani-is-hidden' );
 
@@ -237,9 +242,10 @@ $( document ).on( 'click', '.js-trigger13', function( e ) {
 
 } );
 
-$( document ).on( 'click', '.js-trigger14', function( e ) {
 
-     $( '.js-target14' ).each( function() {
+$( document ).on( 'click', '.js-fil-ani-FlipLeft180-trigger', function( e ) {
+
+     $( '.js-fil-ani-FlipLeft180-target' ).each( function() {
 
          if ( $( this ).hasClass( 'fil-ani-card__flipped' ) ) {
               $( this ).removeClass( 'fil-ani-delay-100 fil-ani-dur-500 fil-ani-FlipLeft180 fil-ani-card__flipped' );
@@ -255,9 +261,10 @@ $( document ).on( 'click', '.js-trigger14', function( e ) {
 } );
 
 
-$( document ).on( 'click', '.js-trigger15', function( e ) {
 
-     $( '.js-target15' ).each( function() {
+$( document ).on( 'click', '.js-fil-ani-PulseScale-trigger', function( e ) {
+
+     $( '.js-fil-ani-PulseScale-target' ).each( function() {
 
           $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-1000 fil-ani-PulseScale' );
 
@@ -268,9 +275,10 @@ $( document ).on( 'click', '.js-trigger15', function( e ) {
 } );
 
 
-$( document ).on( 'click', '.js-trigger16', function( e ) {
 
-     $( '.js-target16' ).each( function() {
+$( document ).on( 'click', '.js-fil-ani-PulseOutline-trigger', function( e ) {
+
+     $( '.js-fil-ani-PulseOutline-target' ).each( function() {
 
           $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-PulseOutline' );
 
@@ -280,9 +288,10 @@ $( document ).on( 'click', '.js-trigger16', function( e ) {
 
 } );
 
-$( document ).on( 'click', '.js-trigger23', function( e ) {
 
-     $( '.js-target23' ).each( function() {
+$( document ).on( 'click', '.js-fil-ani-Shake-trigger', function( e ) {
+
+     $( '.js-fil-ani-Shake-target' ).each( function() {
 
           $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-1200 fil-ani-Shake' );
 
@@ -292,9 +301,10 @@ $( document ).on( 'click', '.js-trigger23', function( e ) {
 
 } );
 
-$( document ).on( 'click', '.js-trigger24', function( e ) {
 
-     $( '.js-target24' ).each( function() {
+$( document ).on( 'click', '.js-fil-ani-Spotlight-trigger', function( e ) {
+
+     $( '.js-fil-ani-Spotlight-target' ).each( function() {
 
           $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-Spotlight' );
           $( '.js-overlay-target' ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-400 fil-ani-fadeIn' );
--- source/js/custom.js
+++ source/js/custom.js
@@ -1,7 +1,8 @@
 
-$( document ).on( 'click', '.js-trigger1', function( e ) {
 
-     $( '.js-target1' ).each( function() {
+$( document ).on( 'click', '.js-fil-ani-fadeIn-trigger', function( e ) {
+
+     $( '.js-fil-ani-fadeIn-target' ).each( function() {
 
           $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeIn fil-ani-is-hidden' );
 
@@ -12,9 +13,10 @@ $( document ).on( 'click', '.js-trigger1', function( e ) {
  } );
 
 
-$( document ).on( 'click', '.js-trigger2', function( e ) {
 
-    $( '.js-target2' ).each( function() {
+$( document ).on( 'click', '.js-fil-ani-fadeInScaleUp-trigger', function( e ) {
+
+    $( '.js-fil-ani-fadeInScaleUp-target' ).each( function() {
 
          $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-500 fil-ani-fadeInScaleUp fil-ani-is-hidden' );
 
@@ -23,6 +25,7 @@ $( document ).on( 'click', '.js-trigger2', function( e ) {
     e.preventDefault();
 
 } );
+
 
 
 $( document ).on( 'click', '.js-fil-ani-fadeInScaleDown-trigger', function( e ) {
@@ -38,6 +41,7 @@ $( document ).on( 'click', '.js-fil-ani-fadeInScaleDown-trigger', function( e ) 
 } );
 
 
+
 $( document ).on( 'click', '.js-fil-ani-fadeInUp-trigger', function( e ) {
 
     $( '.js-fil-ani-fadeInUp-target' ).each( function() {
@@ -49,6 +53,7 @@ $( document ).on( 'click', '.js-fil-ani-fadeInUp-trigger', function( e ) {
     e.preventDefault();
 
 } );
+
 
 
 $( document ).on( 'click', '.js-fil-ani-fadeInDown-trigger', function( e ) {
@@ -64,9 +69,10 @@ $( document ).on( 'click', '.js-fil-ani-fadeInDown-trigger', function( e ) {
 } );
 
 
-$( document ).on( 'click', '.js-trigger3', function( e ) {
 
-     $( '.js-target3' ).each( function() {
+$( document ).on( 'click', '.js-fil-ani-fadeInLeft-trigger', function( e ) {
+
+     $( '.js-fil-ani-fadeInLeft-target' ).each( function() {
 
           $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeInLeft fil-ani-is-hidden' );
 
@@ -77,9 +83,10 @@ $( document ).on( 'click', '.js-trigger3', function( e ) {
  } );
 
 
- $( document ).on( 'click', '.js-trigger4', function( e ) {
 
-      $( '.js-target4' ).each( function() {
+ $( document ).on( 'click', '.js-fil-ani-fadeInRight-trigger', function( e ) {
+
+      $( '.js-fil-ani-fadeInRight-target' ).each( function() {
 
            $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeInRight fil-ani-is-hidden' );
 
@@ -89,9 +96,10 @@ $( document ).on( 'click', '.js-trigger3', function( e ) {
 
 } );
 
-$( document ).on( 'click', '.js-trigger5', function( e ) {
 
-     $( '.js-target5' ).each( function() {
+$( document ).on( 'click', '.js-fil-ani-FlipUp90-trigger', function( e ) {
+
+     $( '.js-fil-ani-FlipUp90-target' ).each( function() {
 
           $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-500 fil-ani-FlipUp90  fil-ani-is-hidden' );
 
@@ -101,9 +109,10 @@ $( document ).on( 'click', '.js-trigger5', function( e ) {
 
 } );
 
-$( document ).on( 'click', '.js-trigger6', function( e ) {
 
-     $( '.js-target6' ).each( function() {
+$( document ).on( 'click', '.js-fil-ani-FlipDown90-trigger', function( e ) {
+
+     $( '.js-fil-ani-FlipDown90-target' ).each( function() {
 
           $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-500 fil-ani-FlipDown90  fil-ani-is-hidden' );
 
@@ -112,23 +121,11 @@ $( document ).on( 'click', '.js-trigger6', function( e ) {
      e.preventDefault();
 
 } );
-//
-// $( document ).on( 'click', '.js-trigger7', function( e ) {
-//
-//      $( '.js-target7' ).each( function() {
-//
-//           $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-500 fil-ani-FlipLeft180' );
-//
-//      } );
-//
-//      e.preventDefault();
-//
-// } );
 
 
-$( document ).on( 'click', '.js-trigger8', function( e ) {
+$( document ).on( 'click', '.js-fil-ani-fadeOut-trigger', function( e ) {
 
-     $( '.js-target8' ).each( function() {
+     $( '.js-fil-ani-fadeOut-target' ).each( function() {
 
           $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeOut fil-ani-is-hidden' );
 
@@ -138,9 +135,10 @@ $( document ).on( 'click', '.js-trigger8', function( e ) {
 
  } );
 
-$( document ).on( 'click', '.js-trigger9', function( e ) {
 
-  $( '.js-target9' ).each( function() {
+$( document ).on( 'click', '.js-fil-ani-fadeOutScaleDown-trigger', function( e ) {
+
+  $( '.js-fil-ani-fadeOutScaleDown-target' ).each( function() {
 
        $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeOutScaleDown fil-ani-is-hidden' );
 
@@ -149,6 +147,7 @@ $( document ).on( 'click', '.js-trigger9', function( e ) {
   e.preventDefault();
 
 } );
+
 
 $( document ).on( 'click', '.js-fil-ani-fadeOutScaleUp-trigger', function( e ) {
 
@@ -162,6 +161,7 @@ $( document ).on( 'click', '.js-fil-ani-fadeOutScaleUp-trigger', function( e ) {
 
 } );
 
+
 $( document ).on( 'click', '.js-fil-ani-fadeOutUp-trigger', function( e ) {
 
   $( '.js-fil-ani-fadeOutUp-target' ).each( function() {
@@ -173,6 +173,7 @@ $( document ).on( 'click', '.js-fil-ani-fadeOutUp-trigger', function( e ) {
   e.preventDefault();
 
 } );
+
 
 $( document ).on( 'click', '.js-fil-ani-fadeOutDown-trigger', function( e ) {
 
@@ -187,9 +188,10 @@ $( document ).on( 'click', '.js-fil-ani-fadeOutDown-trigger', function( e ) {
 } );
 
 
-$( document ).on( 'click', '.js-trigger10', function( e ) {
 
-     $( '.js-target10' ).each( function() {
+$( document ).on( 'click', '.js-fil-ani-fadeOutLeft-trigger', function( e ) {
+
+     $( '.js-fil-ani-fadeOutLeft-target' ).each( function() {
 
           $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeOutLeft fil-ani-is-hidden' );
 
@@ -200,9 +202,10 @@ $( document ).on( 'click', '.js-trigger10', function( e ) {
 } );
 
 
-$( document ).on( 'click', '.js-trigger11', function( e ) {
 
-     $( '.js-target11' ).each( function() {
+$( document ).on( 'click', '.js-fil-ani-fadeOutRight-trigger', function( e ) {
+
+     $( '.js-fil-ani-fadeOutRight-target' ).each( function() {
 
           $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-fadeOutRight fil-ani-is-hidden' );
 
@@ -213,9 +216,10 @@ $( document ).on( 'click', '.js-trigger11', function( e ) {
 } );
 
 
-$( document ).on( 'click', '.js-trigger12', function( e ) {
 
-     $( '.js-target12' ).each( function() {
+$( document ).on( 'click', '.js-fil-ani-FlipOutUp90-trigger', function( e ) {
+
+     $( '.js-fil-ani-FlipOutUp90-target' ).each( function() {
 
           $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-500 fil-ani-FlipOutUp90 fil-ani-is-hidden' );
 
@@ -225,9 +229,10 @@ $( document ).on( 'click', '.js-trigger12', function( e ) {
 
 } );
 
-$( document ).on( 'click', '.js-trigger13', function( e ) {
 
-     $( '.js-target13' ).each( function() {
+$( document ).on( 'click', '.js-fil-ani-FlipOutDown90-trigger', function( e ) {
+
+     $( '.js-fil-ani-FlipOutDown90-target' ).each( function() {
 
           $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-500 fil-ani-FlipOutDown90 fil-ani-is-hidden' );
 
@@ -237,9 +242,10 @@ $( document ).on( 'click', '.js-trigger13', function( e ) {
 
 } );
 
-$( document ).on( 'click', '.js-trigger14', function( e ) {
 
-     $( '.js-target14' ).each( function() {
+$( document ).on( 'click', '.js-fil-ani-FlipLeft180-trigger', function( e ) {
+
+     $( '.js-fil-ani-FlipLeft180-target' ).each( function() {
 
          if ( $( this ).hasClass( 'fil-ani-card__flipped' ) ) {
               $( this ).removeClass( 'fil-ani-delay-100 fil-ani-dur-500 fil-ani-FlipLeft180 fil-ani-card__flipped' );
@@ -255,9 +261,10 @@ $( document ).on( 'click', '.js-trigger14', function( e ) {
 } );
 
 
-$( document ).on( 'click', '.js-fil-ani-PulseOutline-trigger', function( e ) {
 
-     $( '.js-fil-ani-PulseOutline-target' ).each( function() {
+$( document ).on( 'click', '.js-fil-ani-PulseScale-trigger', function( e ) {
+
+     $( '.js-fil-ani-PulseScale-target' ).each( function() {
 
           $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-1000 fil-ani-PulseScale' );
 
@@ -268,9 +275,10 @@ $( document ).on( 'click', '.js-fil-ani-PulseOutline-trigger', function( e ) {
 } );
 
 
+
 $( document ).on( 'click', '.js-fil-ani-PulseOutline-trigger', function( e ) {
 
-     $( '.js-target16' ).each( function() {
+     $( '.js-fil-ani-PulseOutline-target' ).each( function() {
 
           $( this ).toggleClass( 'fil-ani-delay-100 fil-ani-dur-300 fil-ani-PulseOutline' );
 
@@ -279,6 +287,7 @@ $( document ).on( 'click', '.js-fil-ani-PulseOutline-trigger', function( e ) {
      e.preventDefault();
 
 } );
+
 
 $( document ).on( 'click', '.js-fil-ani-Shake-trigger', function( e ) {
 
@@ -291,6 +300,7 @@ $( document ).on( 'click', '.js-fil-ani-Shake-trigger', function( e ) {
      e.preventDefault();
 
 } );
+
 
 $( document ).on( 'click', '.js-fil-ani-Spotlight-trigger', function( e ) {
 
